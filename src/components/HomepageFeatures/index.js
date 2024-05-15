830425097cff9358e46f503708b5a6f4e4a17146
MarinaPolis <polisukmarina476@gmail.com>
2024-05-15 23:22:44 +0300

    First Commit

---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,61 @@
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+const FeatureList = [
+  {
+    title: 'Унікальний стиль',
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+        Стиль поєднує класичні техніки з сучасними тенденціями, створюючи неповторні зображення
+      </>
+    ),
+  },
+  {
+    title: 'Професійний досвід',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    description: (
+      <>
+        З багаторічним досвідом роботи в різних жанрах фотографії, від портретів до пейзажів
+      </>
+    ),
+  },
+  {
+    title: 'Персоналізований підхід',
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        Кожен клієнт для Олександра - це історія, яку він хоче розповісти через фотографії. 
+      </>
+    ),
+  },
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
