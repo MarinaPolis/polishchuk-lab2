8af8beff3e63578e8dbeedf3c99588b7938d7c2f
MarinaPolis <polisukmarina476@gmail.com>
2024-05-15 23:43:32 +0300

    Add items.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://MarinaPolis.github.io',
+  url: 'https://github.com/MarinaPolis',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/polishchuk-lab2/',
